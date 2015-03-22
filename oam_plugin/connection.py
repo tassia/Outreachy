f5423c50434aae84227af57fd8b15a800560c0ce
Tassia Camoes Araujo <tassia@riseup.net>
2015-03-22 04:04:43 -0400

    SSHConnection class

---
+++ oam_plugin/connection.py
@@ -0,0 +1,43 @@
+import os, paramiko
+
+class SSHConnection(object):
+    """
+    Connect and transfer files over SSH using paramiko library
+    """
+
+    def __init__(self, host, username, password, port=22):
+        """
+        Initialize and setup connection
+        """
+        self.ssh = None
+        self.isOpen = False
+        self.transport = paramiko.Transport((host, port))
+        self.transport.connect(username=username, password=password)
+
+    def _openConnection(self):
+        """
+        Open a connection if not already open
+        """
+        if not self.isOpen:
+            self.ssh = paramiko.SFTPClient.from_transport(self.transport)
+            self.isOpen = True
+
+    def push(self, local_path, remote_path = None):
+        """
+        Copie a file from the local host to the remote host user directory
+        """
+        self._openConnection()
+        if not remote_path:
+            remote_path = os.path.expanduser("~") +'/'+ os.path.basename(unicode(local_path))
+        print 'Local path:', local_path
+        print 'Remote path: ', remote_path
+        self.ssh.put(local_path, remote_path)
+
+    def close(self):
+        """
+        Close SSH connection
+        """
+        if self.isOpen:
+            self.ssh.close()
+            self.isOpen = False
+        self.transport.close()
